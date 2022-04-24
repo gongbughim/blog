92101e32fb1a3a659ea1e0b273617d88102c9acb
gongbughim <gongbughim@users.noreply.github.com>
2022-04-24 00:06:42 +0000

    deploy: 87146519cb0e6806e449301bea7ebdd8273d1ce1

---
+++ _app/pages/__layout.svelte-6c6ca375.js
@@ -0,0 +1,23 @@
+import{S as ae,i as le,s as se,F as re,e as i,t as g,k as B,G as ne,c as o,a as h,h as y,d as t,m as F,b as a,H as l,g as I,I as ie,J as oe,K as ce,r as fe,p as ue,L as me}from"../chunks/index-1c92eaba.js";import{c as T}from"../chunks/singletons-d1fb5791.js";import{c as J}from"../chunks/conf-cbd2bb0f.js";T.disable_scroll_handling;T.goto;T.invalidate;T.prefetch;T.prefetch_routes;T.before_navigate;const he=T.after_navigate;function de(d){let c,f,_,L,v,r,p,u,w,M,P,A,E,x,D,b,G,m,U=J.copyright+"",C,H,k,R,$,j,q,N;const O=d[3].default,n=re(O,d,d[2],null);return{c(){c=i("link"),f=i("link"),_=i("script"),L=g(`;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : ''
+      j.async = true
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+      f.parentNode.insertBefore(j, f)
+    })(window, document, 'script', 'dataLayer', 'GTM-PHD2KSF')`),v=B(),r=i("nav"),p=i("ul"),u=i("li"),w=i("a"),M=g("\uBE14\uB85C\uADF8 \uD648"),P=B(),A=i("li"),E=i("a"),x=g("\uC18C\uAC1C"),D=B(),b=i("main"),n&&n.c(),G=B(),m=i("footer"),C=g(U),H=g(`,
+  `),k=i("a"),R=g("Twitter"),$=g(` |
+  `),j=i("a"),q=g("Github"),this.h()},l(e){const s=ne('[data-svelte="svelte-1bmcg5l"]',document.head);c=o(s,"LINK",{rel:!0,type:!0,title:!0,href:!0}),f=o(s,"LINK",{rel:!0,type:!0,title:!0,href:!0}),_=o(s,"SCRIPT",{});var V=h(_);L=y(V,`;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : ''
+      j.async = true
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+      f.parentNode.insertBefore(j, f)
+    })(window, document, 'script', 'dataLayer', 'GTM-PHD2KSF')`),V.forEach(t),s.forEach(t),v=F(e),r=o(e,"NAV",{class:!0});var z=h(r);p=o(z,"UL",{class:!0});var K=h(p);u=o(K,"LI",{});var Q=h(u);w=o(Q,"A",{"sveltekit:prefetch":!0,href:!0});var W=h(w);M=y(W,"\uBE14\uB85C\uADF8 \uD648"),W.forEach(t),Q.forEach(t),P=F(K),A=o(K,"LI",{});var X=h(A);E=o(X,"A",{"sveltekit:prefetch":!0,href:!0});var Y=h(E);x=y(Y,"\uC18C\uAC1C"),Y.forEach(t),X.forEach(t),K.forEach(t),z.forEach(t),D=F(e),b=o(e,"MAIN",{});var Z=h(b);n&&n.l(Z),Z.forEach(t),G=F(e),m=o(e,"FOOTER",{class:!0});var S=h(m);C=y(S,U),H=y(S,`,
+  `),k=o(S,"A",{href:!0,class:!0});var ee=h(k);R=y(ee,"Twitter"),ee.forEach(t),$=y(S,` |
+  `),j=o(S,"A",{href:!0,class:!0});var te=h(j);q=y(te,"Github"),te.forEach(t),S.forEach(t),this.h()},h(){a(c,"rel","alternate"),a(c,"type","application/rss+xml"),a(c,"title","RSS"),a(c,"href","/rss.xml"),a(f,"rel","sitemap"),a(f,"type","application/xml"),a(f,"title","Sitemap"),a(f,"href","/sitemap.xml"),a(w,"sveltekit:prefetch",""),a(w,"href","/"),a(E,"sveltekit:prefetch",""),a(E,"href","/posts/about"),a(p,"class","svelte-1y5881k"),a(r,"class","gnb svelte-1y5881k"),a(k,"href","https://twitter.com/"+J.authorTwitter),a(k,"class","external"),a(j,"href","https://github.com/"+J.authorGithub),a(j,"class","external"),a(m,"class","svelte-1y5881k")},m(e,s){l(document.head,c),l(document.head,f),l(document.head,_),l(_,L),I(e,v,s),I(e,r,s),l(r,p),l(p,u),l(u,w),l(w,M),l(p,P),l(p,A),l(A,E),l(E,x),I(e,D,s),I(e,b,s),n&&n.m(b,null),d[4](b),I(e,G,s),I(e,m,s),l(m,C),l(m,H),l(m,k),l(k,R),l(m,$),l(m,j),l(j,q),N=!0},p(e,[s]){n&&n.p&&(!N||s&4)&&ie(n,O,e,e[2],N?ce(O,e[2],s,null):oe(e[2]),null)},i(e){N||(fe(n,e),N=!0)},o(e){ue(n,e),N=!1},d(e){t(c),t(f),t(_),e&&t(v),e&&t(r),e&&t(D),e&&t(b),n&&n.d(e),d[4](null),e&&t(G),e&&t(m)}}}function pe(d,c,f){let{$$slots:_={},$$scope:L}=c,v=0,r=null;he(()=>f(1,v=Math.random()*360));function p(u){me[u?"unshift":"push"](()=>{r=u,f(0,r)})}return d.$$set=u=>{"$$scope"in u&&f(2,L=u.$$scope)},d.$$.update=()=>{d.$$.dirty&3&&r&&r.ownerDocument.firstElementChild.style.setProperty("--c-primary-hue",`${v|0}`)},[r,v,L,_,p]}class ye extends ae{constructor(c){super();le(this,c,pe,de,se,{})}}export{ye as default};
