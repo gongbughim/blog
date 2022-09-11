6d80dd5f323030a321c56a28b391daee84c59743
gongbughim <gongbughim@gmail.com>
2022-09-11 12:09:09 +0900

    fix: prefetch 개선
    과거 방식(`sveltekit:prefetch`) 대신 새 방식(`data-sveltekit-prefetch`) 사용

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -4,7 +4,6 @@ import rehypeKatexSvelte from 'rehype-katex-svelte'
 import rehypeSlug from 'rehype-slug'
 import remarkAbbr from 'remark-abbr'
 import remarkMath from 'remark-math'
-import { visit } from 'unist-util-visit'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -16,22 +15,7 @@ const config = {
     dashes: 'oldschool',
   },
   remarkPlugins: [remarkMath, remarkAbbr],
-  rehypePlugins: [
-    rehypeKatexSvelte,
-    rehypeSlug,
-    [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-    () => {
-      // Attach "sveltekit:prefetch" to internal links
-      return tree => {
-        const pInnerLink = /^(\/|\.)/
-        visit(tree, 'element', node => {
-          if (node.tagName !== 'a') return
-          if (!node.properties.href.match(pInnerLink)) return
-          node.properties['sveltekit:prefetch'] = 'sveltekit:prefetch'
-        })
-      }
-    },
-  ],
+  rehypePlugins: [rehypeKatexSvelte, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
   layout: {
     _: path.join(__dirname, './src/components/LayoutDefault.svelte'),
   },
